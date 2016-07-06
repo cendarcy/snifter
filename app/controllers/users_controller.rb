ee76436dfb6ca9da48393fd679be78ac7e46a0c0
Camille Davis <camilleedavis@gmail.com>
2016-07-06 19:54:22 -0400

    users created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end	
+
+	def new
+		@user = User.new
+	end
+	
+	def create
+		User.create(fname: params[:user][:fname], 
+			lname: params[:user][:lname], 
+			bday: params[:user][:bday], 
+			email: params[:user][:email])
+
+		redirect_to users_path
+	end
 end
